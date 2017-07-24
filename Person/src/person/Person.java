e957cd0fc061c7c6c192193487e2674731a5d3fa
UltimatePhase <orthancdestroyer@gmail.com>
2017-07-24 10:43:39 -0700

    person

---
+++ Person/src/person/Person.java
@@ -0,0 +1,31 @@
+
+
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class Person {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println ("My name is Tyler King.");
+        System.out.println ("I am 14 yeasr old");
+        System.out.println ("I was born in San Jose, and about half my life was spent in San Jose while the other half was spent in Newbury Park.");
+        System.out.println ("My hobbies are mostly computers and some games.");
+        System.out.println ("My favorite color changes from time to time, but it normally is blue, white, or a shade of those colors.");
+        
+    }
+    
+}
