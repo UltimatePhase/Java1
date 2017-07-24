771573bd9518ea145d32650fe564d38a7b9a9a62
UltimatePhase <orthancdestroyer@gmail.com>
2017-07-24 11:32:00 -0700

    person

---
+++ ARandomProject/src/ARandomProject.java
@@ -0,0 +1,26 @@
+import java.util.Scanner
+
+package arandomproject;
+
+/**
+ *
+ * @author Education Unlimited
+ */
+public class ARandomProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("We will be comparing our ages. I will write my age as the variable x and you will write your age as the variable y.");
+        int x;
+        int y; 
+        if (x > y)  {
+       System.out.println("HAHA! I'm older than you.");
+}
+        else{
+            System.out.println("Dang it! I'm younger.");        
+                    }        
+    }
+}
+
--- Person2/src/person2/Person2.java
+++ Person2/src/person2/Person2.java
@@ -3,21 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package arandomproject;
+package person2;
 
 /**
  *
  * @author Education Unlimited
  */
-public class ARandomProject {
+public class Person2 {
 
     /**
      * @param args the command line arguments
      */
-
-        int x; 
-        if (x > 50) {
+    public static void main(String[] args) {
+        int age = 14;
+        double height = 5.75;
+        String firstName = "Tyler";
+        String lastName = "King";
+        boolean Astudent = true;
+        System.out.println("My name is" + );
         
     }
     
-
+}
