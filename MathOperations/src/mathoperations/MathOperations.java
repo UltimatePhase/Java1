19076f492a59a781ebc9835c36718ee33789bd1a
UltimatePhase <orthancdestroyer@gmail.com>
2017-07-24 11:50:17 -0700

    MathOperations

---
+++ MathOperations/src/mathoperations/MathOperations.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mathoperations;
+
+public class MathOperations {
+     public static void main( String[] args ) {
+         int p,q,r,s,t,u;
+          double a,b,c,v,w,x,y,z;
+          String first, second, together;
+ 
+          p = 8;
+          q = 25;
+         System.out.println( "p is " + p + ", q is " + q );
+ 
+         r = p + q;
+         System.out.println( "p+q is " + r );
+         s = p - q;
+         System.out.println( "p-q is " + s );
+         t = p+q*3;
+         System.out.println( "p+q*3 is " + t );
+         u = q / 2;
+         System.out.println( "q/2 is " + u );
+         v = p+(12121)/15;
+         System.out.println("(p + 12121)/15 is" + v);
+        w = p+(121121)/15;
+         System.out.println("(p + 121121)/15 is" + w); 
+        x = p+(1211121)/15;
+         System.out.println("(p + 1211121)/15 is" + x);
+        y = p+(12111121)/15;
+         System.out.println("(p + 12111121)/15 is" + y);
+           z = p+(1221)/15;
+         System.out.println("(p + 1221)/15 is" + z);
+        
+        a = 1.1;
+         System.out.println( "a is " + a );
+         b = a*a;
+         System.out.println( "a*a is " + b );
+         c = b / 2;
+         System.out.println( "b/2 is " + c );
+         System.out.println();
+
+         first = "peanut butter";
+         second = " and jelly";
+         together = first + second;
+         System.out.println( together );
+         
+     }
+}
+/**
+ *
+ * @author Education Unlimited
+
+        // TODO code application logic here
+    }
+    
+}
